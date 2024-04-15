d1edafa1d2da53d18c9975ce9a3eb9a0ba614fd8
ytraub <y.traub@outlook.com>
2024-04-15 22:35:02 +0200

    more cleanup

--- runtime/src/main.rs
+++
@@ -1,9 +0,0 @@
-/* use ev3dev_lang_rust::motors::{LargeMotor, MotorPort};
-use ev3dev_lang_rust::sensors::{ColorSensor, SensorPort}; */
-use ev3dev_lang_rust::{Ev3Error, Ev3Result};
-
-mod utils;
-
-fn main() -> Ev3Result<()> {
-    Ok(())
-}
--- runtime/upload.sh
+++
@@ -1,2 +0,0 @@
-cargo build --release
-scp -C -r target/armv5te-unknown-linux-musleabi/release/runtime robot@ev3dev:/home/robot/mindfactory-code-test/runtime
